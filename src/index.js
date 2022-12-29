857a19442620d694524c94e784cdbb6aef87cc23
Amaan Ahmad <amaansaifi262@gmail.com>
2022-12-29 16:38:30 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
+  // <BrowserRouter basename="/movieTown"> // to host on github 
   <BrowserRouter>
     <App />
   </BrowserRouter>
